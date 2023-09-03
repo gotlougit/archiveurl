8333e8cf5224d6501c3886648469fcae8802612c
Saksham Mittal <gotlouemail@gmail.com>
2023-09-03 12:36:50 +0530

    Clean up shell script and regex

--- archiveurl.sh
+++ archiveurl.sh
@@ -2,19 +2,20 @@
 
 # A small script to automatically archive webpages liked to in webpages
 
-files=$(ls -1 *.html *.md)
+files=$(ls -1 ./*.html ./*.md)
 url=""
 
 for file in $files
 do
-	url=$url$(grep --only-matching "https://[a-zA-Z0-9./?=_%:-]*" $file)
+	url=$url$(grep --only-matching "https://[a-zA-Z0-9./?=_%:-]*" "$file")
 done
 
-url=$(echo -e $url | tr " " '\n' | sort -u)
+url=$(echo -e "$url" | tr " " '\n' | sort -u)
+echo "$url"
 
 for u in $url
 do
 	echo Archiving "$u"
-	curl -I "http://web.archive.org/save/"$u
+	curl -I "http://web.archive.org/save/""$u"
 	sleep 2
 done 
